7dd007f852c4330432a08f2d8a14a6e5cff4ad1c
unknown <samueloctavianus97@gmail.com>
2022-05-06 17:42:51 +0700

    another js update

---
+++ day2-movie-seat-booking/day2.js
@@ -0,0 +1,25 @@
+//look the value up
+let movieSelection = document.querySelector("#movie-select");
+let finalSeats = document.querySelector("#final-seats");
+let finalPrice = document.querySelector("#final-price");
+let seats = document.querySelectorAll(".seats-selection > .row >  .seat");
+
+//get total seats that are selected
+//combine it with the movie & current cost
+//display total seats & price
+
+seats.forEach((seat) => {
+  seat.addEventListener("click", () => {
+    //count & update selected seats
+    seat.classList.toggle("selected");
+    let selectedSeats = document.querySelectorAll(".row > .selected");
+    console.log(`Total Selected Seats: ${selectedSeats.length}`);
+    finalSeats.innerText = selectedSeats.length;
+  });
+});
+
+//count & show the total price
+movieSelection.addEventListener("change", (e) => {
+  let moviePrice = e.target.value;
+  finalPrice.innerText = `$${moviePrice}`;
+});
