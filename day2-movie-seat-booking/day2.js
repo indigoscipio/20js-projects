a3f95672443188af8cd4b8150b7df7977c97d9f9
unknown <samueloctavianus97@gmail.com>
2022-05-07 18:18:42 +0700

    day3 added

--- day2-movie-seat-booking/day2.js
+++ day2-movie-seat-booking/day2.js
@@ -2,28 +2,56 @@
 let movieSelection = document.querySelector("#movie-select");
 let finalSeats = document.querySelector("#final-seats");
 let finalPrice = document.querySelector("#final-price");
-let seats = document.querySelectorAll(".seats-selection > .row >  .seat");
+let seatContainer = document.querySelector(".seats-selection");
+let seats = document.querySelectorAll(
+  ".seats-selection > .row >  .seat:not(.occupied)"
+);
+let ticketPrice = +movieSelection.value;
 
 //get total seats that are selected
 //combine it with the movie & current cost
 //display total seats & price
 
-seats.forEach((seat) => {
-  seat.addEventListener("click", () => {
-    //count & show the total price
-    movieSelection.addEventListener("change", (e) => {
-      let moviePrice = e.target.value;
-      finalPrice.innerText = `$${moviePrice * selectedSeats.length}`;
-    });
-
-    if (seat.classList.contains("occupied") == true) {
-      console.log("this seat is occupied!");
-    }
-
-    //count & update selected seats
-    seat.classList.toggle("selected");
-    let selectedSeats = document.querySelectorAll(".row > .selected");
-    console.log(`Total Selected Seats: ${selectedSeats.length}`);
-    finalSeats.innerText = selectedSeats.length;
-  });
+seatContainer.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+  }
+  updateSeatsCount();
+  console.log(e.target);
 });
+
+//update seats & ticket price
+function updateSeatsCount() {
+  //Update seats count
+  let selectedSeats = document.querySelectorAll(".row > .selected");
+  console.log(`Total Selected Seats: ${selectedSeats.length}`);
+  finalSeats.innerText = selectedSeats.length;
+  finalPrice.innerText = `$${ticketPrice * selectedSeats.length}`;
+}
+
+//update movie
+movieSelection.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  console.log(ticketPrice);
+  updateSeatsCount();
+});
+
+// seats.forEach((seat) => {
+//   seat.addEventListener("click", () => {
+//     //Update seats count
+//     seat.classList.toggle("selected");
+//     let selectedSeats = document.querySelectorAll(".row > .selected");
+//     console.log(`Total Selected Seats: ${selectedSeats.length}`);
+//     finalSeats.innerText = selectedSeats.length;
+//     finalPrice.innerText = `$${ticketPrice * selectedSeats.length}`;
+//   });
+// });
+
+// //Movie dropdown
+// movieSelection.addEventListener("change", (e) => {
+//   let moviePrice = +e.target.value;
+//   finalPrice.innerText = `$${moviePrice * selectedSeats.length}`;
+// });
