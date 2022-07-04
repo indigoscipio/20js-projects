84a21e0af1028a0cf7997f034119fd318e2afa60
unknown <samueloctavianus97@gmail.com>
2022-07-04 19:54:59 +0700

    got stock

--- day14-memory-card/script.js
+++ day14-memory-card/script.js
@@ -1,58 +1,115 @@
-//users can flip each card to show its back content
-//useres can swipe/slide each card
-//users can add a new card by clicking the add card button
-// when the add card button is clicked, a modal will popup.
-
 let btnPrev = document.querySelector(".arrow-left");
 let btnNext = document.querySelector(".arrow-right");
+let mainContainer = document.querySelector(".main");
 let cards = document.querySelectorAll(".flip-card");
+let btnClose = document.querySelector(".icon-close");
+let btnAdd = document.querySelector(".button--add");
+let btnSubmit = document.querySelector("#submit");
 let cardSlider = document.querySelector(".card-slider");
 let pagination = document.querySelector(".pagination");
+let modalContainer = document.querySelector(".modal-container");
+let modalEl = document.querySelector(".modal");
+let taQuestion = document.querySelector("#question");
+let taAnswer = document.querySelector("#answer");
 
+//initial variables
 let idx = 0;
 let space = 1.6;
+let cardsData = [];
 
-btnNext.addEventListener("click", slideRight);
-btnPrev.addEventListener("click", slideLeft);
+// function slideRight() {
+//   idx++;
+//   if (idx > cards.length - 1) {
+//     idx = 0;
+//   }
 
-function slideRight() {
-  idx++;
-  if (idx > cards.length - 1) {
-    idx = 0;
+//   console.log(idx);
+
+//   //slide the card to the right
+//   cardSlider.style.transform = `translateX(calc(${-100 * idx}% - ${
+//     space * idx
+//   }rem))`;
+
+//   //Set or unflip all cards to default
+//   unFlipCard();
+// }
+
+// function slideLeft() {
+//   idx--;
+//   if (idx < 0) {
+//     idx = cards.length - 1;
+//   }
+//   console.log(idx);
+
+//   //slide the card to the left
+//   cardSlider.style.transform = `translateX(calc(${-100 * idx}% - ${
+//     space * idx
+//   }rem))`;
+
+//   //Set or unflip all cards to default
+//   unFlipCard();
+// }
+
+// function unFlipCard() {
+//   let cb = document.querySelectorAll(`input[type="checkbox"]:checked`);
+//   cb.forEach((el) => {
+//     if (el.checked) {
+//       el.checked = false;
+//     }
+//   });
+// }
+
+function toggleModal() {
+  modalContainer.classList.toggle("active");
+}
+
+function addCard(e) {
+  e.preventDefault();
+
+  //empty trim validation
+  if (!taQuestion.value.trim() || !taAnswer.value.trim()) {
+    alert("Can't be emptry!");
+  } else {
+    toggleModal();
+
+    let newCardData = {
+      id: Date.now(),
+      question: taQuestion.value,
+      answer: taAnswer.value,
+    };
+
+    //push the data to card data
+    cardsData.push(newCardData);
+    console.log(cardsData);
+
+    //Create card container
+    let cardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container");
   }
-
-  console.log(idx);
-
-  //slide the card to the right
-  cardSlider.style.transform = `translateX(calc(${-100 * idx}% - ${
-    space * idx
-  }rem))`;
-
-  //Set or unflip all cards to default
-  unFlipCard();
 }
 
-function slideLeft() {
-  idx--;
-  if (idx < 0) {
-    idx = cards.length - 1;
-  }
-  console.log(idx);
+function createSliderInfo() {
+  let sliderInfo = document.createElement("div");
+  sliderInfo.classList.add("slider-info");
+  sliderInfo.innerHTML = `
+        <button class="arrow-left">
+            <i class="fa-solid fa-arrow-left"></i>
+        </button>
+        <p class="pagination">1/3</p>
+        <button class="arrow-right">
+             <i class="fa-solid fa-arrow-right"></i>
+        </button>`;
 
-  //slide the card to the left
-  cardSlider.style.transform = `translateX(calc(${-100 * idx}% - ${
-    space * idx
-  }rem))`;
-
-  //Set or unflip all cards to default
-  unFlipCard();
+  mainContainer.append(sliderInfo);
 }
 
-function unFlipCard() {
-  let cb = document.querySelectorAll(`input[type="checkbox"]:checked`);
-  cb.forEach((el) => {
-    if (el.checked) {
-      el.checked = false;
-    }
-  });
+function deleteAllCards() {
+  return true;
 }
+
+//Event Listeners
+// btnNext.addEventListener("click", slideRight);
+// btnPrev.addEventListener("click", slideLeft);
+btnClose.addEventListener("click", toggleModal);
+btnAdd.addEventListener("click", toggleModal);
+modalEl.addEventListener("submit", addCard);
